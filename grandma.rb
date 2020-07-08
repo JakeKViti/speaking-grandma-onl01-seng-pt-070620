b0c36860734774ab2d4cbc0412998e2737d073b6
Jacob Viti <thevitij64@gmail.com>
2020-07-08 02:47:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,15 +13,16 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
+
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
+
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   end
 
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  end
-
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
